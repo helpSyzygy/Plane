527d18b0ae9957313104304d298a345ff3ca218d
helpSyzygy <42008244+helpSyzygy@users.noreply.github.com>
2018-08-04 17:33:48 +0300

    п

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerCharacter : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 4f;
+    public float force = 300f;
+    public float rotateSpeed = 300f;
+    public Transform target;
+    public Rigidbody2D rbTarget;
+    public Vector2 startPos;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPos = GetComponent<Transform>().position;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.right).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.right * speed;
+
+        transform.position = new Vector3(startPos.x, rb.transform.position.y, transform.position.z);
+
+        target.position = new Vector2(transform.position.x + 4, target.position.y);
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+         
+        }
+        if (collision.gameObject.tag == "Level")
+        {
+            
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Const.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const : MonoBehaviour {
+
+    int coin;
+    int coin_max;
+    int dollars;
+    int dollars_max;
+    int score;
+    int score_max;
+    bool Game;
+    bool day;
+
+}
